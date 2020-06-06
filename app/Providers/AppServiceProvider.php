d769c6ac26f9d48f13ceff388b9098f42650e5c9
cloudsample6 <cloudsample6@gmail.com>
2020-06-07 00:15:51 +0700

    update string length DB

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
+use Illuminate\Database\Schema\Builder; // Import Builder where defaultStringLength method is defined
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,5 +28,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         View::share('appName', 'Internet Broadband V2');
+        Builder::defaultStringLength(191); // Update defaultStringLength
+
     }
 }
