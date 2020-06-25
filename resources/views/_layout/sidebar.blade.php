4802f6fafe50c0ddd8a57d36573acb100947c835
cloudsample6 <cloudsample6@gmail.com>
2020-06-25 19:40:01 +0700

    Add url dashboard

--- resources/views/_layout/sidebar.blade.php
+++ resources/views/_layout/sidebar.blade.php
@@ -8,7 +8,7 @@
           <ul class="sidebar-menu">
               <li class="menu-header">Dashboard</li>
               <li class="nav-item dropdown">
-                <a href="#"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+                <a href="{{route ('dashboard-index')}}"><i class="fas fa-fire"></i><span>Dashboard</span></a>
                 <!-- <ul class="dropdown-menu">
                   <li><a class="nav-link" href="index-0.html">General Dashboard</a></li>
                   <li><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
