eaf21f1fbe7256bb6daa613abd6ac0489498c832
cloudsample6 <cloudsample6@gmail.com>
2020-08-12 12:07:33 +0700

    Edit Icon

--- resources/views/_layout/app.blade.php
+++ resources/views/_layout/app.blade.php
@@ -7,6 +7,7 @@
   <title>@yield('title', '') | {{ $appName }}</title>
   <!-- CSS -->
   <link rel="stylesheet" type="text/css" href="{{asset('assets/vendors/jqueryui/css/jquery-ui.min.css')}}">
+  <link rel="shortcut icon" href="../assets/img/logo2.ico" type="image/x-icon" />
   
   <!-- Script -->
   <script src="{{asset('/assets/vendors/jquery/js/jquery-3.3.1.min.js')}}" type="text/javascript"></script>
@@ -58,7 +59,7 @@
 
   <!-- General JS Scripts -->
   
-  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <!-- <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script> -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
