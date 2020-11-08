3a19a237448b8fc54383bb7183b9c4175c57a02b
cloudsample6 <cloudsample6@gmail.com>
2020-11-08 12:35:43 +0700

    fix bug

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -97,7 +97,7 @@
                                 <h1 class="mb-2 display-4 font-weight-bold">Selamat Datang!</h1>
                                 <h5 class="font-weight-normal text-muted-transparent">Nikmati Internet Tanpa Batas!</h5>
                             </div>
-                            Photo by <a class="text-light bb" target="_blank" href="https://unsplash.com/photos/a8lTjWJJgLA">Justin Kauffman</a> on <a class="text-light bb" target="_blank" href="https://unsplash.com">Unsplash</a>
+                            <!-- Photo by <a class="text-light bb" target="_blank" href="https://unsplash.com/photos/a8lTjWJJgLA">Justin Kauffman</a> on <a class="text-light bb" target="_blank" href="https://unsplash.com">Unsplash</a> -->
                         </div>
                     </div>
                 </div>
