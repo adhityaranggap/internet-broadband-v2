305d9a59f1eeca4f1e72d0c6904c7c244cd3ad74
cloudsample6 <cloudsample6@gmail.com>
2020-12-04 00:16:43 +0700

    edit fix bug login on app

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Exceptions;
-use Illuminate\Auth\AuthenticationException;
+
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -38,14 +38,7 @@ class Handler extends ExceptionHandler
     {
         parent::report($exception);
     }
-    protected function unauthenticated($request, AuthenticationException $exception)
-    {
-        if ($request->expectsJson()) {
-            return response()->json(['error' => 'Unauthenticated.'], 401);
-        }
-     
-        return redirect()->guest('/');
-    }
+
     /**
      * Render an exception into an HTTP response.
      *
